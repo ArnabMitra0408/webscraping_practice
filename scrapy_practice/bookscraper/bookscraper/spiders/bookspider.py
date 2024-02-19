c6064a4f9edf07c3e2d25b10b58f0f62ba8e6b81
Arnab Mitra <arnabmitra490@gmail.com>
2024-02-19 15:01:33 +0530

    created bookscraper

--- scrapy_practice/bookscraper/bookscraper/spiders/bookspider.py
+++ scrapy_practice/bookscraper/bookscraper/spiders/bookspider.py
@@ -2,9 +2,23 @@ import scrapy
 
 
 class BookspiderSpider(scrapy.Spider):
-    name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
+    name = "bookspider"  #name of the spider
+    allowed_domains = ["books.toscrape.com"] #we can only scrape from the domains present in this list
+    start_urls = ["https://books.toscrape.com"] #this would be our starting point.
 
     def parse(self, response):
-        pass
+        books=response.css('article.product_pod')
+        for book in books:
+           yield {
+               'name':book.css('h3 a::text').get(),
+               'price': book.css('div.product_price').css('p.price_color::text').get(),
+               'url': books.css('h3 a').attrib['href']
+           }
+        next_page=response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            next_page_url='https://books.toscrape.com/'+next_page
+            yield response.follow(next_page_url,callback=self.parse)
+
+
+
+
